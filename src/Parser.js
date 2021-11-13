560eebad68653f97742a6ca629449721e5c474c5
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2021-11-14 00:56:36 +0300

    feat: argv parser

---
+++ src/Parser.js
@@ -0,0 +1,82 @@
+// TODO: move common code in a function
+class Parser {
+  parse(argv) {
+    const args = argv.slice(2);
+
+    return {
+      config: this.#getConfig(args),
+      input: this.#getInput(args),
+      output: this.#getOutput(args),
+    };
+  }
+
+  #CONFIG_OPTION = ['-c', '--config'];
+
+  #INPUT_OPTIONS = ['-i', '--input'];
+
+  #OUTPUT_OPTIONS = ['-o', '--output'];
+
+  #getConfig(args) {
+    const isConfigOption = (arg) => this.#CONFIG_OPTION.includes(arg);
+
+    const optionCount = args.filter(isConfigOption).length;
+
+    if (optionCount > 1) {
+      throw new Error(
+        'Too many config options, please provide only one "-c" or "--config" option!',
+      );
+    }
+
+    if (!optionCount) {
+      throw new Error(
+        'You haven\'t written config option, please provide "-c" or "--config" option!',
+      );
+    }
+
+    const index = args.findIndex(isConfigOption);
+
+    return args[index + 1];
+  }
+
+  #getInput(args) {
+    const isInputOption = (arg) => this.#INPUT_OPTIONS.includes(arg);
+    const optionCount = args.filter(isInputOption).length;
+
+    if (optionCount > 1) {
+      throw new Error('Too many config options, please provide only one "-i" or "--input" option!');
+    }
+
+    if (!optionCount) {
+      throw new Error(
+        'You haven\'t written config option, please provide "-i" or "--input" option!',
+      );
+    }
+
+    const index = args.findIndex(isInputOption);
+
+    return args[index + 1];
+  }
+
+  #getOutput(args) {
+    const isOutputOption = (arg) => this.#OUTPUT_OPTIONS.includes(arg);
+    const optionCount = args.filter(isOutputOption).length;
+
+    if (optionCount > 1) {
+      throw new Error(
+        'Too many config options, please provide only one "-o" or "--output" option!',
+      );
+    }
+
+    if (!optionCount) {
+      throw new Error(
+        'You haven\'t written config option, please provide "-o" or "--output" option!',
+      );
+    }
+
+    const index = args.findIndex(isOutputOption);
+
+    return args[index + 1];
+  }
+}
+
+module.exports = Parser;
