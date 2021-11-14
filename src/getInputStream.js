1f6b895e619401dceca531b99fd638f2a7faf00d
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2021-11-14 13:47:03 +0300

    feat: input and output streams

---
+++ src/getInputStream.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+async function getReadStream(file) {
+  if (!file) {
+    console.log("You don't write input file, will use stdin");
+    return process.stdin;
+  }
+
+  try {
+    await fs.accessSync(file, fs.constants.R_OK);
+
+    return fs.createReadStream(file);
+  } catch {
+    throw new Error("Can't get access to input file");
+  }
+}
+
+module.exports = getReadStream;
